6e737e85dfe3d48d5301bb74d079d85c89cf1053
Svitlana Apolokhova <37361118+cweet-dreams@users.noreply.github.com>
2023-01-29 22:50:09 +0200

    project  weather(ui) added

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import Forecast from './Forecast';
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="weather-wrap border-bottom">
+        <form id="search-form">
+          <div className="row mb-3">
+            <div className="col-10">
+              <input
+                type="search"
+                placeholder="Type a city .."
+                className="form-control"
+              />
+            </div>
+            <div className="col-2">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="weather-info">
+          <div className="row">
+            <div className="col-6 d-flex">
+              <img alt="partly_cloudy" src='https://ssl.gstatic.com/onebox/weather/64/sunny.png' />
+              <strong>12</strong>
+              <span className="units">
+                <a className="active-unit">°C </a>
+              </span>
+
+              <ul className="weather-points">
+                <li>Humidity: 74%</li>
+                <li>Wind: 3 km/h</li>
+              </ul>
+            </div>
+            <div className="col-6 text-end city-info">
+              <h1>Lisbon, PT </h1>
+              <ul>
+                <li>Last updated: Sunday 14:13</li>
+                <li>Clear</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='weather-forecast'>
+          
+            <Forecast />
+         
+          </div>
+        
+      </div>
+      <small>        <a href="https://github.com/cweet-dreams/weather-app-vanilla"> Open-source code </a> by Svetlana Apolokhova</small>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Forecast.js
@@ -0,0 +1,60 @@
+import React from "react";
+import './App.css';
+
+export default function forecast() {
+    let days = [
+        {name: 'Sunday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        },
+        {name: 'Monday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        },
+        {name: 'Tuesday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        },
+        {name: 'Wednesday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        },
+        {name: 'Thursday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        },
+        {name: 'Friday',
+        icon: 'https://ssl.gstatic.com/onebox/weather/48/sunny.png',
+        day: 10,
+        night: 5
+        }   
+      ];
+    
+
+    return (
+        <div className='row'>
+        
+            {days.map(function(day, index) {
+                     return (
+                    <div className="col-2">
+                        <div key={index}>            
+                            <div className="weather-forecast-date">{day.name}</div>
+                                <img  src={day.icon} alt="weather-pic"  width="32"/>
+                                <div className="weather-forecast-temperatures">
+                                <span className="weather-forecast-temperature-max">{day.day} ℃ </span> /
+                                <span className="weather-forecast-temperature-min">{day.night} ℃ </span>
+                            </div>
+                        </div>  
+                    </div>  
+            );}
+            
+            )}
+           
+        </div>
+    );
+}
